66cc18ae9d5a56f54d92bcdd6b071e5251974d54
krikaz <pascal.bou@protonmail.com>
2019-07-26 10:16:48 +0200

    added login

--- config/routes.js
+++ config/routes.js
@@ -14,6 +14,10 @@ async function add(user) {
 	return findById(id);
 }
 
+function findBy(filter) {
+	return db('users').where(filter);
+}
+
 function findById(id) {
 	return db('users')
 		.where({ id })
@@ -35,7 +39,25 @@ function register(req, res) {
 }
 
 function login(req, res) {
-	// implement user login
+  let { username, password } = req.body;
+
+	findBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				// const token = generateToken(user);
+
+				res.status(200).json({
+					message: `Welcome ${user.username}!`,
+					// token,
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 }
 
 function getJokes(req, res) {
